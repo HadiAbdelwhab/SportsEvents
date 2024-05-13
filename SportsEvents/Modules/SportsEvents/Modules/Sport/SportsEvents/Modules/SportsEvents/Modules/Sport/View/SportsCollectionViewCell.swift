55adc64069675729bb2da2297da88545d8b69484
AbdulHameed Mohamed <59577985+AbdulHameedMohamed@users.noreply.github.com>
2024-05-13 15:43:13 +0300

    Setup View in Sports Feature

--- SportsEvents/Modules/SportsEvents/Modules/Sport/SportsEvents/Modules/SportsEvents/Modules/Sport/View/SportsCollectionViewCell.swift
+++ SportsEvents/Modules/SportsEvents/Modules/Sport/SportsEvents/Modules/SportsEvents/Modules/Sport/View/SportsCollectionViewCell.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class SportsCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var ivImage: UIImageView!
+    @IBOutlet weak var tvName: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func bindSport(sport: Sport) {
+        ivImage.image = UIImage(named: sport.thumb)
+        tvName.text = sport.title
+    }
 }
--- SportsEvents/Modules/SportsEvents/Modules/Sport/SportsEvents/Modules/SportsEvents/Modules/Sport/View/SportsViewController.swift
+++ SportsEvents/Modules/SportsEvents/Modules/Sport/SportsEvents/Modules/SportsEvents/Modules/Sport/View/SportsViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class SportsViewController: UIViewController {
 
+    var viewModel = SportsViewModel()
+    
+    @IBOutlet weak var cvSports: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewModel.onSportClicked = { [weak self] sport in
+            self?.handleSportClicked(for: sport)
+        }
+                
+        viewModel.onSportsFetched = { [weak self] in
+            self?.updateCollectionView()
+        }
+        
+        let nib = UINib(nibName: "cSport", bundle: nil)
+        cvSports.register(nib, forCellWithReuseIdentifier: "cSport")
     }
+    
+    func updateCollectionView() {
+        cvSports.reloadData()
+    }
+    
+    func handleSportClicked(for sport: Sport) {
+        // Go To League Screen
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.dataSource.getSports().count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cSport", for: indexPath) as! SportsCollectionViewCell
 
-
+        cell.bindSport(sport: viewModel.dataSource.getSports()[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let sport = viewModel.dataSource.getSports()[indexPath.row]
+        viewModel.handleSportClicked(for: sport)
+    }
 }
 
