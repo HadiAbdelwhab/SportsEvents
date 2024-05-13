0b0df9f966e8cae0069c3a26192c28a895897791
Hadi_Abdelwhab <105137729+HadiAbdelwhab@users.noreply.github.com>
2024-05-13 05:54:24 -0700

    added league details method to viewMode
    setup getLeatestResults and getUpcomingEvents

--- SportsEvents/Data/Model/SportsDependencyProvider.swift
+++ SportsEvents/Data/Model/SportsDependencyProvider.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+
+class SportsDependencyProvider {
+    private static let apiService = SportsApi()
+ 
+
+    static func provideLeagueDetailsViewModel() -> LeagueDetailsViewModel {
+        return LeagueDetailsViewModel(apiService: self.apiService)
+    }
+}
